23124187efbc706150bb728805d789cebd7fdfbc
arthur <372251027@qq.com>
2024-12-05 00:11:57 +0800

    feat: 添加飞船移动速度

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """初始化飞船并设置初始位置"""
 
         self.screen = alien_invasion.screen
+        self.settings = alien_invasion.settings
 
         # 加载飞船图标，并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +16,9 @@ class Ship:
 
         # 飞船初始化放在屏幕底部的中央
         self.rect.midbottom = self.screen.get_rect().midbottom
+
+        # 这里必须要浮点数，不然+=可能不变
+        self.x = float(self.rect.x)
 
         # 移动标志
         self.moving_right = False
@@ -26,6 +30,8 @@ class Ship:
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        # 根据浮点数更新rect坐标
+        self.rect.x = self.x
